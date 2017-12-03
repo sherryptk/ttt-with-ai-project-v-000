d70951de1946ea0f122190433c15afe124b2fe2d
Sherry El Garhy <sherryptk@gmail.com>
2017-12-03 14:31:27 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,12 +6,12 @@ module Players
       move = nil
       if !board.taken?(5)
         move = "5"
-      else
+      elsif !board.taken?(1) || !board.taken?(3) || !board.taken?(7) || !board.taken?(9)
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
       else
         move = [2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s
       end
-      
+
     end
 
    end
